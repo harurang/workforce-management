f8dedf25fe47ed83b6c52b50b874806968779440
Hillary Arurang <hillary.arurang@ge.com>
2017-03-18 22:02:48 -0500

    Added Insert sql script for person, phone_number, soc, naics and nwcet

---
+++ sql-scripts/insert.sql
@@ -0,0 +1,182 @@
+INSERT INTO person
+VALUES (2, 'Jane', 'Metairie', '3325 Causway', 'LA', 70001, 'jane@yahoo.com', 'female');
+
+INSERT INTO person
+VALUES (4, 'John', 'New Orleans', '9532 Carolyn', 'LA', 70131, 'john@gmail.com', 'male');
+
+INSERT INTO person
+VALUES (5, 'Adam', 'Houston', '8834 Sugar', 'LA', 53908, 'adam@yahoo.com', 'male');
+
+INSERT INTO person
+VALUES (7, 'Vanessa', 'Houston', '0324 Hill', 'TX', 12874, 'vanessa@aol.com', 'female');
+
+INSERT INTO person
+VALUES (9, 'Lindsey', 'San Diego', '3423 Severn', 'CA', 94665, 'lindsey@yahoo.com', 'female');
+
+
+
+INSERT INTO phone_number
+VALUES (2, '702-324-2342', '124-854-2342');
+
+INSERT INTO phone_number
+VALUES (4, '123-435-2343', '856-345-6572');
+
+INSERT INTO phone_number
+VALUES (5, '234-634-2342', '786-234-5435');
+
+INSERT INTO phone_number
+VALUES (7, '644-234-4532', '567-234-5463');
+
+INSERT INTO phone_number
+VALUES (9, '234-323-2342', '657-345-4562');
+
+
+
+INSERT INTO soc 
+VALUES ('Computer and Information Research Scientists', 'Computer and Information Research Scientists', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+INSERT INTO soc 
+VALUES ('Computer Systems Analysts', 'Computer and Information Analysts', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+INSERT INTO soc 
+VALUES ('Information Security Analysts', 'Computer and Information Analysts', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+INSERT INTO soc 
+VALUES ('Computer Programmers', 'Software Developers and Programmers', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+INSERT INTO soc 
+VALUES ('Software Developers, Applications', 'Software Developers and Programmers', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+INSERT INTO soc 
+VALUES ('Web Developers', 'Software Developers and Programmers', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+INSERT INTO soc 
+VALUES ('Computer User Support Specialists', 'Computer Support Specialists', 'Computer Occupations', 'Computer and Mathematical Occupations');
+
+
+
+INSERT INTO naics 
+VALUES (54, 'Professional, Scientific, and Technical Services', null);
+
+INSERT INTO naics 
+VALUES (541, 'Professional, Scientific, and Technical Services', 54);
+
+INSERT INTO naics 
+VALUES (5415, 'Computer Systems Design and Related Services', 541);
+
+INSERT INTO naics 
+VALUES (54151, 'Computer Systems Design and Related Services', 5415);
+
+INSERT INTO naics 
+VALUES (541511, 'Custom Computer Programming Services', 5415);
+
+INSERT INTO naics 
+VALUES (541512, 'Computer Systems Design Services', 5415);
+
+INSERT INTO naics 
+VALUES (541513, 'Computer Facilities Management Services', 5415);
+
+INSERT INTO naics 
+VALUES (541519, 'Other Computer Related Services', 5415);
+
+
+
+INSERT INTO nwcet
+VALUES ('Visual Basic for Applications (VBA)');
+
+INSERT INTO nwcet
+VALUES ('jquery');
+
+INSERT INTO nwcet
+VALUES ('Visual Studio');
+
+INSERT INTO nwcet
+VALUES ('VMware');
+
+INSERT INTO nwcet
+VALUES ('Jenkins');
+
+INSERT INTO nwcet
+VALUES ('JAVA');
+
+INSERT INTO nwcet
+VALUES ('Git');
+
+INSERT INTO nwcet
+VALUES ('JavaScript');
+
+INSERT INTO nwcet
+VALUES ('Oracle PL/SQL');
+
+INSERT INTO nwcet
+VALUES ('PeopleSoft');
+
+INSERT INTO nwcet
+VALUES ('PERL');
+
+INSERT INTO nwcet
+VALUES ('Ruby on Rails');
+
+INSERT INTO nwcet
+VALUES ('Spring Framework');
+
+INSERT INTO nwcet
+VALUES ('UNIX');
+
+INSERT INTO nwcet
+VALUES ('Wireshark');
+
+INSERT INTO nwcet
+VALUES ('Swift');
+
+INSERT INTO nwcet
+VALUES ('Ubuntu');
+
+INSERT INTO nwcet
+VALUES ('SQL');
+
+INSERT INTO nwcet
+VALUES ('Red Hat Linux');
+
+INSERT INTO nwcet
+VALUES ('Python');
+
+INSERT INTO nwcet
+VALUES ('Objective-C');
+
+INSERT INTO nwcet
+VALUES ('JSON');
+
+INSERT INTO nwcet
+VALUES ('Django');
+
+INSERT INTO nwcet
+VALUES ('CSS');
+
+INSERT INTO nwcet
+VALUES ('C++');
+
+INSERT INTO nwcet
+VALUES ('Amazon Web Services (AWS)');
+
+INSERT INTO nwcet
+VALUES ('COBOL');
+
+INSERT INTO nwcet
+VALUES ('Cisco Routers');
+
+INSERT INTO nwcet
+VALUES ('Eclipse');
+
+INSERT INTO nwcet
+VALUES ('Google Analytics');
+
+
+
+
+
+
+
+
+
+
