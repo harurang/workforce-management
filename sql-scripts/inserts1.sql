82c410f3a94c65f83a0ab55e9ee66c340e63a7ae
Eresha Polite <epolite@uno.edu>
2017-03-28 15:35:51 -0500

    initial commit

---
+++ sql-scripts/inserts1.sql
@@ -0,0 +1,180 @@
+INSERT INTO knowledge_skill
+VALUES (435782, 'Database Development and Administration', 'Tier II', 'SQL');
+
+INSERT INTO knowledge_skill
+VALUES (435783, 'Digital Media'. 'Tier II', 'CSS, HTML5, Javascript');
+
+INSERT INTO knowledge_skill
+VALUES (435784, 'Enterprise Systems Analysis and Integration', 'Tier II', 'SQL');
+
+INSERT INTO knowledge_skill
+VALUES (435785, 'Network Design and Administration', 'Tier II', 'PERL, Red Hat Linux');
+
+INSERT INTO knowledge_skill
+VALUES (435786, 'Programming/Software Engineering', 'Tier II', 'Java, C++, Python' );
+
+INSERT INTO knowledge_skill
+VALUES (435787, 'Techinal Support', 'Tier I', null );
+
+INSERT INTO knowledge_skill
+VALUES (435788, 'Technical Writing', 'Tier I', null );
+
+INSERT INTO knowledge_skill
+VALUES (435789, 'Web Development and Administration', 'Tier II', 'Javascript, CSS, HTML5, jQuery, Python' );
+
+
+
+
+INSERT INTO course_knowledge
+VALUES (435782, 1543 );
+
+INSERT INTO course_knowledge
+VALUES (435783, 2106);
+
+INSERT INTO course_knowledge
+VALUES (435784, 2114);
+
+INSERT INTO course_knowledge
+VALUES (435785, 2345);
+
+INSERT INTO course_knowledge
+VALUES (435786, 3024);
+
+INSERT INTO course_knowledge
+VALUES (435786, 3120);
+
+INSERT INTO course_knowledge
+VALUES (435788, 4102);
+
+INSERT INTO course_knowledge
+VALUES (435789, 4208);
+
+
+
+
+INSERT INTO paid_by
+VALUES (5, );
+
+INSERT INTO paid_by
+VALUES (6, );
+
+INSERT INTO paid_by
+VALUES (7, );
+
+INSERT INTO paid_by
+VALUES (8, );
+
+INSERT INTO paid_by
+VALUES (9, );
+
+
+
+
+INSERT INTO takes
+VALUES (5, 321);
+
+INSERT INTO takes
+VALUES (6, 322);
+
+INSERT INTO takes
+VALUES (7, 323);
+
+INSERT INTO takes
+VALUES (8, 324);
+
+INSERT INTO takes
+VALUES (9, 325);
+
+
+
+
+
+INSERT INTO job_history
+VALUES ('02/10/2017', '03/10/2015', , 5);
+
+INSERT INTO job_history
+VALUES ('03/08/2017', '01/23/2014', , 6);
+
+INSERT INTO job_history
+VALUES ('01/14/2017', '05/16/2015', , 7);
+
+INSERT INTO job_history
+VALUES ('03/14/2017', '01/09/2016', , 8);
+
+INSERT INTO job_history
+VALUES ('02/21/2017', '04/21/2013', , 9);
+
+
+
+
+
+INSERT INTO section
+VALUES (321,'', '',  );
+
+INSERT INTO section
+VALUES (322, '', '',  );
+
+INSERT INTO section
+VALUES (323, '', '', );
+
+INSERT INTO section
+VALUES (324, '', '', );
+
+INSERT INTO section
+VALUES (325, '', '', );
+
+
+
+
+
+INSERT INTO course
+VALUES (1543, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (2106, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (2114, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (2345, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (3024, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (3120, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (4102, '', '', '', '', '', '');
+
+INSERT INTO course
+VALUES (4208, '', '', '', '', '', '');
+
+
+
+
+
+INSERT INTO tier
+VALUES ('Tier III', 'Industry-specific technical skills, knowledge, and
+abilities unique to individual industries and organization. Examples: 
+knowledge of and compliance with companies and organization protocols, 
+understanding and effective use of industry terminology, knowledge of and 
+compliance with industry legal requirements, and knowledge of and compliance
+with company and product standards.');
+
+INSERT INTO tier
+VALUES ('Tier II', 'Technical skills, knowledge, and abilities skills common 
+to all jobs within a career cluster across all industries. Examples: 
+proficient use of software and hardware tools, proficient use of Internet
+techniques, understanding of hardware/system architecture, and troubleshooting
+of software and hardware problems.');
+
+INSERT INTO tier
+VALUES ('Tier I', 'The set of foundation skills (SCANS), knowledge, abilities,
+and personal qualities required of all workers to be successful in today's
+workplace. Examples: basic skills (reading, writing, arithmetic, etc.),
+thinking skills, personal qualities, management of time and resources,
+interpersonal skills, management and use of information, understanding and
+management of systems, and use of technology.');
+
