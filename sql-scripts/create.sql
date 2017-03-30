c5091adc33d3bd0081744588a1cd8a0ac6214534
Hillary Arurang <hillary.arurang@ge.com>
2017-03-29 22:25:34 -0500

    Statements are ordered so tables can be deleted, created and inserted within minutes

--- sql-scripts/create.sql
+++ sql-scripts/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE person (
-  per_id varchar(3),
+  per_id number,
   name varchar(30),
   city varchar(30),
   street varchar(30),
@@ -11,10 +11,25 @@ CREATE TABLE person (
 );
 
 CREATE TABLE phone_number (
-  per_id varchar(3),
+  per_id number,
   home varchar(30),
-  work varchar(30)
+  work varchar(30),
   foreign key (per_id) references person(per_id)
+);
+
+CREATE TABLE soc (
+  soc_title varchar(70),
+  primary key (soc_title)
+);
+
+CREATE TABLE job_category (
+  cate_code number,
+  parent_cate varchar(70),
+  pay_range_high varchar(40),
+  pay_range_low varchar(40),
+  soc_title varchar(70),
+  primary key (cate_code),
+  foreign key (soc_title) references soc(soc_title)
 );
 
 CREATE TABLE job (
@@ -25,6 +40,16 @@ CREATE TABLE job (
   job_title varchar(70),
   primary key (job_code),
   foreign key (cate_code) references job_category
+);
+
+CREATE TABLE course(
+    c_code number,
+    course_level varchar(70),
+    description varchar(70),
+    status varchar(70),
+    price varchar(70),
+    prereq varchar(70),
+    primary key (c_code) 
 );
 
 CREATE TABLE section (
@@ -38,11 +63,6 @@ CREATE TABLE section (
   foreign key (c_code) references course(c_code)
 );
 
-CREATE TABLE soc (
-  soc_title varchar(70),
-  primary key (soc_title)
-);
-
 CREATE TABLE takes (
   sec_id number,
   per_id number,
@@ -51,9 +71,9 @@ CREATE TABLE takes (
 );
 
 CREATE TABLE job_history (
-  per_id varchar(3),
+  per_id number,
   job_code number,
-  start_date varchar(50),l
+  start_date varchar(50),
   end_date varchar(50),
   foreign key (per_id) references person(per_id),
   foreign key (job_code) references job(job_code)
@@ -61,7 +81,7 @@ CREATE TABLE job_history (
 
 CREATE TABLE naics (
   n_code number,
-  title varchar(70),
+  title varchar(200),
   hierarchy number,
   primary key (n_code),
   foreign key (hierarchy) references naics(n_code)
@@ -75,16 +95,6 @@ CREATE TABLE nwcet (
 CREATE TABLE career_cluster (
   cluster_title varchar(70),
   primary key (cluster_title)
-);
-
-CREATE TABLE job_category (
-  cate_code number,
-  soc_title varchar(70),
-  parent_cate varchar(70),
-  pay_range_high varchar(40),
-  pay_range_low varchar(40),
-  primary key (cate_code),
-  foreign key (soc_title) references soc(soc_title)
 );
 
 CREATE TABLE company (
@@ -130,19 +140,24 @@ CREATE TABLE paid_by(
   foreign key (job_code) references job(job_code)
 );
 
-CREATE TABLE course(
-    c_code number,
-    course_level varchar(70),
-    description varchar(70),
-    status varchar(70),
-    price varchar(70),
-    prereq varchar(70),
-    primary key (c_code) 
+CREATE TABLE person_skill (
+  per_id number,
+  ks_code number,
+  foreign key (per_id) references person(per_id),
+  foreign key (ks_code) references knowledge_skill(ks_code)
 );
 
-CREATE TABLE comp_job(
+CREATE TABLE job_skill (
+  job_code number,
+  ks_code number,
+  importance varchar(30),
+  foreign key (job_code) references job(job_code),
+  foreign key (ks_code) references knowledge_skill(ks_code)
+);
+
+CREATE TABLE comp_job (
     comp_id number,
     job_code number,
-    foreign key comp_id references company(comp_id),
-    foreign key job_code references job(job_code)
+    foreign key (comp_id) references company(comp_id),
+    foreign key (job_code) references job(job_code)
 );
---
+++ sql-scripts/delete.sql
@@ -0,0 +1,43 @@
+drop table phone_number;
+
+drop table job_history;
+
+drop table job_history;
+
+drop table comp_job;
+
+drop table company;
+
+drop table naics;
+
+drop table nwect;
+
+drop table course_knowledge;
+
+drop table job_skill;
+
+drop table person_skill;
+
+drop table knowledge_skill;
+
+drop table career_cluster;
+
+drop table career_tier;
+
+drop table paid_by;
+
+drop table job;
+
+drop table job_category;
+
+drop table soc;
+
+drop table nwcet;
+
+drop table takes;
+
+drop table person;
+
+drop table section;
+
+drop table course;
